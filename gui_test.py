fe42887b09fbbd0630c0b204750034723d5b5536
jcbellamy <57716864+jcbellamy@users.noreply.github.com>
2019-11-15 14:37:38 -0500

    Add files via upload

--- gui_test.py
+++ gui_test.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 import os
 from day_nightv2 import main
 dirname =""
-filename="/home/jbellamy/darknet/build/darknet/x64/data/images/April 2019/04280001.JPG"
+filename="/home/jbellamy/Desktop/Senior Lab Project/Test Images/04280001.JPG"
 w, h = 400,400
 #sets working directory, separate from button and only use as helper
 def find_dir():
@@ -38,7 +38,7 @@ def find_bounding_boxes(file_str,can,label_obj,rec):
         label_obj.set(label_obj.get()+class_names[obj]+"("+str(obj_counter)+") ")
         obj_counter+=1
 #opens and displays image file
-def find_file(i_c):
+def find_file(i_c,label_obj):
     global filename
     filename = askopenfilename()
     if filename is None or not filename:
@@ -49,6 +49,7 @@ def find_file(i_c):
     i = i.resize((w, h), Image.ANTIALIAS)
     root.p =ImageTk.PhotoImage(i)
     curr_img=i_c.create_image(w/2,h/2,image=root.p)
+    label_obj.set("");
 #runs darknet train command, switch for classify program in the future
 def run_darknet():
     os.chdir("/home/jbellamy/darknet")
@@ -62,16 +63,15 @@ c=Canvas(root, width=w,height=h)
 i=c.create_image(w/2,h/2,image=root.photo)
 c.pack()
 f = Frame(root)
-
 find_dir_b=Button(f,text="Find Directory",command=find_dir)
 find_dir_b.grid(row=0,column=0)
 class_b=Button(f,text="Split",command= lambda : main(dirname))
 class_b.grid(row=0,column=1)
-open_img=Button(f,text="Open Image",command= lambda: find_file(c))
-open_img.grid(row=0,column=2)
 st = StringVar()
 img_class = Label(f, textvariable=st)
 st.set("Label: ")
+open_img=Button(f,text="Open Image",command= lambda: find_file(c,st))
+open_img.grid(row=0,column=2)
 img_class.grid(row=1,column=0)
 draw_box=Button(f,text="Draw Box",command= lambda : find_bounding_boxes(filename.split(".JPG")[0]+".txt",c,st,i))
 draw_box.grid(row=0,column=3)
